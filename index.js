6b61acb3b6e0b34e83018ec0cc21e990164351df
gerry <weirich@elexis.ch>
2019-02-12 18:13:09 +0100

    first run

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const knex=require('knex')
 const cfg=require('config')
+const processor=require('./process')
 
 const source=knex({
   client: cfg.get('source.client'),
   connection: cfg.get('source.connection')
 })
 
-/*
-const dest=knex({
+
+const prov=knex({
   client: cfg.get('dest.client'),
   connection: cfg.get('dest.connection')
 })
-*/
 
-source.select("id").from('kontakt').then(patids=>{
-  const num=patids.lenth
+const destdb=cfg.get('dest.connection.database')
+prov.raw("DROP DATABASE "+destdb).then(res=>{
+  return res
+}).then(res=>{
+  return  prov.raw("CREATE DATABASE "+destdb).then(res=>{
+    return res
+  })
+}).then(res=>{
+
+  const dest=knex({
+    client: cfg.get('dest.client'),
+    connection: cfg.get('dest.connection')
+  })
+  
+  processor(source,dest,cfg.get('process')).then(result=>{
+    console.log(result)
+  }).catch(err=>{
+    console.log("***FEHLER*** "+err)
+  })
+
 })
+  
+
