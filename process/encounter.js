a726e305b33a5df2992154feefe303359011614f
gerry <weirich@elexis.ch>
2019-02-13 16:01:35 +0100

    bugfix

--- process/encounter.js
+++ process/encounter.js
@@ -1,5 +1,5 @@
 const { dest, checkinsert, checktransfer } = require('../db')
-const { checkKontakt } = require('./kontakt')
+const checkKontakt  = require('./kontakt')
 
 const transfer = async (encounter) => {
   await checkKontakt(encounter.mandantid)
