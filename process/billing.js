ca670514342030c692de638fea212ed2b32b6c6b
rgwch <weirich@elexis.ch>
2019-02-20 15:51:33 +0100

    add billings

---
+++ process/billing.js
@@ -0,0 +1,57 @@
+/********************************************
+ * This file is part of Make-Demodb         *
+ * Copyright (c) 2019 by G. Weirich         *
+ * License and terms: see LICENSE           *
+ ********************************************/
+
+const { source, checkinsert, checktransfer } = require('../db')
+const checkKontakt = require('./kontakt')
+const log = require("../logger")
+
+const transfer = async leistung => {
+    if (leistung) {
+        await checkinsert('leistungen', leistung)
+        if (leistung.userid) {
+            await checkKontakt(leistung.userid)
+        }
+        let table = ""
+        switch (leistung.klasse) {
+            case "at.medevit.ch.artikelstamm.elexis.common.ArtikelstammItem":
+            case "ch.artikelstamm.elexis.common.ArtikelstammItem":
+                table = "artikelstamm_ch"; break;
+            case "ch.elexis.data.Artikel":    
+            case "ch.elexis.core.eigenartikel.Eigenartikel":    
+            case "ch.elexis.artikel_ch.data.Medical":
+            case "ch.elexis.artikel_ch.data.Medikament":
+            case "ch.elexis.data.Medical":
+            case "ch.elexis.data.Medikament":
+            case "ch.elexis.eigenartikel.Eigenartikel":
+            case "ch.elexis.artikel_ch.data.MiGelArtikel":
+            case "ch.elexis.medikamente.bag.data.BAGMedi":
+            case "ch.elexis.data.Eigenartikel":
+                table = "artikel"; break;
+            case "ch.elexis.data.LaborLeistung":
+                table = "ch_medelexis_labortarif2009"; break;
+            case "ch.elexis.data.TarmedLeistung":
+                table = "tarmed"; break;
+            case "ch.elexis.labortarif2009.data.Labor2009Tarif":
+            case "ch.elexis.data.LaborLeistung":
+                table = "ch_medelexis_labortarif2009"; break;
+            case "ch.elexis.privatrechnung.data.Leistung ":
+                table = "ch_elexis_privatrechnung"; break;
+            case "ch.berchtold.emanuel.privatrechnung.data.Leistung":
+                table="ch_berchtold_privatrechnung"; break;    
+            case "ch.elexis.data.Eigenleistung":
+                table="eigenleistungen"; break;    
+            case "ch.elexis.data.PhysioLeistung":
+                table="ch_elexis_arzttarife_ch_physio"; break;    
+            default:
+                log.warn("unknown billing class ", leistung.klasse)
+        }
+        await checktransfer(table, leistung.leistg_code)
+    } else {
+        log.warn("undefined billing")
+    }
+}
+
+module.exports = transfer
--- process/encounter.js
+++ process/encounter.js
@@ -5,7 +5,8 @@
  ********************************************/
 
 const { source, checkinsert, checktransfer } = require('../db')
-const checkKontakt  = require('./kontakt')
+const checkKontakt = require('./kontakt')
+const checkBilling = require("./billing")
 
 /**
  * process an encounter and handle dependend objects such as 'leistungen' and 'diagnosen'.
@@ -15,14 +16,14 @@ const transfer = async (encounter) => {
   await checkKontakt(encounter.mandantid)
   await checktransfer("rechnungen", encounter.rechnungsid)
   await checkinsert("behandlungen", encounter)
-  const leistungen=await source('leistungen').where('behandlung',encounter.id)
-  for(const leistung of leistungen){
-    await checkinsert('leistungen',leistung)
+  const leistungen = await source('leistungen').where('behandlung', encounter.id)
+  for (const leistung of leistungen) {
+    await checkBilling(leistung)
   }
-  const dgs=await source('behdl_dg_joint').where('behandlungsid',encounter.id)
-  for(const dg of dgs){
-    await checkinsert('behdl_dg_joint',dg)
-    await checktransfer('diagnosen',dg.diagnoseid)
+  const dgs = await source('behdl_dg_joint').where('behandlungsid', encounter.id)
+  for (const dg of dgs) {
+    await checkinsert('behdl_dg_joint', dg)
+    await checktransfer('diagnosen', dg.diagnoseid)
   }
 }
 
